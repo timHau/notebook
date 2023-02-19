dddac46e2e4416f98eae77e85ebd67067c9a9005
timHau <tim.hau@hotmail.de>
2023-02-19 21:40:15 +0100

    remove unused css themes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,8 @@ import './index.css'
 import store from './store/store';
 import { Provider } from 'react-redux';
 import "prismjs/components/prism-python";
-import "./assets/themes/prism-duotone-dark.css";
+
+import "./assets/themes/prism-duotone-forest.css";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
--- client/src/notebookView/Cell.tsx
+++ client/src/notebookView/Cell.tsx
@@ -1,7 +1,7 @@
 // @ts-ignore-next-line
 import { highlight, languages } from "prismjs/components/prism-core";
 import { CellBindingProps, CellEditorProps, CellProps } from "../types";
-import { RxPlay } from "react-icons/rx";
+import { RxPlay, RxMagicWand, RxLinkBreak1, RxPencil1 } from "react-icons/rx";
 import Api from "../api/api";
 import { updateBinding } from "../store/cellSlice";
 import { useAppSelector, useAppDispatch } from "../store/hooks";
@@ -26,8 +26,6 @@ function Cell(props: CellProps) {
         return <div>Loading...</div>
     }
 
-    console.log(languages)
-
     return (
         <div>
             <CellEditor cell={cell} handleEval={handleEval} />
@@ -39,6 +37,7 @@ function Cell(props: CellProps) {
 function CellEditor(props: CellEditorProps) {
     const { cell } = props;
     const [localCode, setLocalCode] = useState<string>(cell.content);
+    const [showCellToolbar, setShowCellToolbar] = useState<boolean>(false);
 
     function handleKeyDown(event: KeyboardEvent) {
         if (event.key === "Enter" && event.shiftKey) {
@@ -48,28 +47,50 @@ function CellEditor(props: CellEditorProps) {
     }
 
     return (
-        <div className="flex items-end my-2">
-            <div>
-                <RxPlay
-                    className={"text-slate-100 mr-1 w-8 h-8 hover:bg-slate-700 hover:cursor-pointer" + (cell.isSynced ? " bg-green-500" : " bg-red-600")}
-                    onClick={() => props.handleEval(localCode)} />
-            </div>
+        <div className="flex flex-col hover:cursor-pointer"
+            onMouseOver={() => setShowCellToolbar(true)}
+            onMouseOut={() => setShowCellToolbar(false)}>
+            <div className="flex items-end my-2 relative">
+                {showCellToolbar &&
+                    <div className="absolute right-0 top-0 z-10">
+                        <RxPlay
+                            className={"mr-1 w-8 h-8 hover:cursor-pointer" + (cell.isSynced ? " text-green-500" : " text-red-600")}
+                            onClick={() => props.handleEval(localCode)} />
+                    </div>
+                }
 
-            <div className="w-full">
-                <Editor
-                    value={localCode}
-                    onValueChange={(code) => setLocalCode(code)}
-                    onKeyDown={handleKeyDown}
-                    highlight={(code) => highlight(code, languages.python, 'python')}
-                    padding={10}
-                    style={{
-                        fontFamily: '"Fira code", "Fira Mono", monospace',
-                        fontSize: 12,
-                        backgroundColor: "#1e1e1e",
-                        color: "#d4d4d4",
-                    }}
-                />
+                <div className="w-full">
+                    <Editor
+                        value={localCode}
+                        onValueChange={(code) => setLocalCode(code)}
+                        onKeyDown={handleKeyDown}
+                        highlight={(code) => highlight(code, languages.python, 'python')}
+                        padding={10}
+                        style={{
+                            fontFamily: '"Fira code", "Fira Mono", monospace',
+                            fontSize: 12,
+                            backgroundColor: "#27272a",
+                            color: "#d4d4d4",
+                            borderRadius: "0.375rem",
+                        }}
+                    />
+                </div>
             </div>
+            {showCellToolbar &&
+                <div className="flex text-xs justify-center">
+                    <div className="flex mr-2">
+                        <RxMagicWand className="text-slate-100 mr-1 w-4 h-4 hover:bg-slate-700 hover:cursor-pointer" />
+                        <span>Reactive code</span>
+                    </div>
+                    <div className="flex mr-2">
+                        <RxLinkBreak1 className="text-slate-100 mr-1 w-4 h-4 hover:bg-slate-700 hover:cursor-pointer" />
+                        <span>Non-reactive code</span>
+                    </div>
+                    <div className="flex">
+                        <RxPencil1 className="text-slate-100 mr-1 w-4 h-4 hover:bg-slate-700 hover:cursor-pointer" />
+                        <span>Markdown</span>
+                    </div>
+                </div>}
         </div>
     )
 }
