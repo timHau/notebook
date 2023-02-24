cf8173ca35856b77a5ec61f4e708c86c585f037c
timHau <tim.hau@hotmail.de>
2023-02-24 17:01:23 +0100

    add cell test

--- src/core/notebook.rs
+++ src/core/notebook.rs
@@ -66,6 +66,7 @@ impl Notebook {
                     &mut scope,
                 )
                 .unwrap(),
+                Cell::new_reactive(TMP, &mut scope).unwrap(),
                 Cell::new_reactive(
                     "import asyncio\n\nasync def main():\n  print('hello')\n\nasyncio.run(main())",
                     &mut scope,
@@ -166,3 +167,59 @@ impl From<&str> for Notebook {
         serde_json::from_str(&json).unwrap()
     }
 }
+
+const TMP: &str = "
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.metrics import classification_report
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras import backend as K
+import matplotlib.pyplot as plt
+import numpy as np
+# import argparse
+
+
+print(\"in main\")
+
+print(\"[INFO] accessing MNIST...\")
+((trainX, trainY), (testX, testY)) = mnist.load_data()
+trainX = trainX.reshape((trainX.shape[0], 28 * 28 * 1))
+testX = testX.reshape((testX.shape[0], 28 * 28 * 1))
+# scale data to the range of [0, 1]
+trainX = trainX.astype(\"float32\") / 255.0
+testX = testX.astype(\"float32\") / 255.0
+
+lb = LabelBinarizer()
+trainY = lb.fit_transform(trainY)
+testY = lb.transform(testY)
+
+model = Sequential()
+model.add(Dense(256, input_shape=(784,), activation=\"sigmoid\"))
+model.add(Dense(128, activation=\"sigmoid\"))
+model.add(Dense(10, activation=\"softmax\"))
+
+print(\"[INFO] training network...\")
+sgd = SGD(0.01)
+model.compile(loss=\"categorical_crossentropy\", optimizer=sgd,
+                metrics=[\"accuracy\"])
+H = model.fit(trainX, trainY, validation_data=(testX, testY),
+                epochs=100, batch_size=128)
+
+print(\"[INFO] evaluating network...\")
+predictions = model.predict(testX, batch_size=128)
+print(classification_report(testY.argmax(axis=1),
+                            predictions.argmax(axis=1),
+                            target_names=[str(x) for x in lb.classes_]))
+
+plt.figure()
+plt.plot(np.arange(0, 100), H.history[\"loss\"], label=\"train_loss\")
+plt.plot(np.arange(0, 100), H.history[\"val_loss\"], label=\"val_loss\")
+plt.plot(np.arange(0, 100), H.history[\"accuracy\"], label=\"train_acc\")
+plt.plot(np.arange(0, 100), H.history[\"val_accuracy\"], label=\"val_acc\")
+plt.title(\"Training Loss and Accuracy\")
+plt.xlabel(\"Epoch #\")
+plt.ylabel(\"Loss/Accuracy\")
+plt.legend()
+";
